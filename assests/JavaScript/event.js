4da5b9ab9c722e1132ac72da40b36cceda2220ff
Shivanandkkhh <Shivanand@shoptrade.co>
2022-12-19 14:49:36 +0530

    The JS added into filters

--- assests/JavaScript/event.js
+++ assests/JavaScript/event.js
@@ -1,6 +1,8 @@
 
+        // the code for filters starts from here
         var expanded=false;
         function showList(){
+
             var list=document.getElementById('checkboxes');
             if(!expanded){
                 list.style.display="block";
@@ -11,4 +13,11 @@
                 expanded=false;
             }
         }
-    
+
+        function checkedFunction(){
+        var checkedInputs = document.querySelectorAll("input:checked");
+         var doc=document.getElementsByClassName('check-number')[0];
+         doc.innerText=checkedInputs.length
+        }
+
+        // the code  form filter sneds from here
