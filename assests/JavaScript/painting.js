cd36fc64752a4f684ed1a671dcecbb03fc627152
Shivanandkkhh <Shivanand@shoptrade.co>
2022-12-29 15:23:27 +0530

    made changies in description

--- assests/JavaScript/painting.js
+++ assests/JavaScript/painting.js
@@ -1,26 +1,49 @@
-function totalClick(click){
-    const totalClick = document.getElementById('totalClick');
-    const sumvalue = parseInt(totalClicks.innerText) + click;
-    console.log(sumvalue + click);
-    totalClicks.innerText =  sumvalue;
 
-    // 
-    if(sumvalue < 0){
-        totalClicks.innerText = 0;
-    }
+// for inceremnet or decrement the number of add cart 
+function totalClick(click) {
+  const totalClick = document.getElementById('totalClick');
+  const sumvalue = parseInt(totalClicks.innerText) + click;
+  console.log(sumvalue + click);
+  totalClicks.innerText = sumvalue;
+  // 
+  if (sumvalue < 0) {
+    totalClicks.innerText = 0;
+  }
 }
 
-// 
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+// for descrpition and review tablinks replacing
+// function openCity(evt, cityName) {
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(cityName).style.display = "block";
+//   evt.currentTarget.className += "active";
+// }
+
+const tabs = document.querySelectorAll(".tablinks");
+const tabcontent = document.querySelectorAll(".tabcontent");
+for (let i = 0; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", () => {
+    for (let j = 0; j < tabcontent.length; j++) {
+      tabcontent[j].classList.remove("tab-content-active")
     }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    for (let k = 0; k < tabs.length; k++) {
+      tabs[k].classList.remove("tab-active")
     }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+    tabcontent[i].classList.add("tab-content-active")
+    tabs[i].classList.add("tab-active")
+  })
+}
+// for replacing the imgies
+function myFunction(img) {
+  var manImg = document.getElementById("replace_img");
+  manImg.src = img.src;
+}
+// document.getElementById("image").classList.add("slide-right");
+// code for replacing tabs?
